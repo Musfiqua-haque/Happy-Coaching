b9114993447698a663e2d0003fd5154f3fa69032
Musfiqua haque <zmushfiqua@gmail.com>
2024-02-11 19:20:16 +0600

    issue with review section

---
+++ index.js
@@ -0,0 +1,9 @@
+function showMobileMenu(){
+    const sidebar = document.querySelector(".sidebar")
+    sidebar.style.display = "flex"
+}
+
+function hideMobileMenu(){
+    const sidebar = document.querySelector(".sidebar")
+    sidebar.style.display = "none"
+}
